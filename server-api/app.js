0ccbacfaa6fa1e2392ffdbb2b015f0ef73928ede
Hady Alkadri <98332279+hadyalkadri@users.noreply.github.com>
2022-11-26 18:49:27 +0300

    Update app.js

--- server-api/app.js
+++ server-api/app.js
@@ -20,6 +20,8 @@ const app = express();
 app.use(cors());
 
 app.use(express.static(path.join(__dirname + "/public")))
+app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
 
 // for parsing form data in format x--www....
 app.use(bodyParser.urlencoded({ extended: false }));
